a6b63702ef9f020269e979582d51635f72162eef
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-09-27 14:01:14 -0400

    double quotation marks exercise added

---
+++ quotation_marks.py
@@ -0,0 +1,15 @@
+# This program checks if the input passed by the user has double quotation marks and }
+# print it without them
+
+def run():
+    string = input('Please enter a phrase: ')
+    def printer(string):
+        if string.startswith('"') and string.endswith('"'):
+            return string[1:-1:]
+        else:
+            return string
+    print(printer(string))
+    return None
+
+if __name__ == "__main__":
+    run()
