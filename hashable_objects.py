410b5764c94447ac84e308946b905c4ea4c1ca0a
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-10-16 11:59:14 -0300

    temporary solution to this problem

---
+++ hashable_objects.py
@@ -0,0 +1,17 @@
+# the object_list has already been defined
+# write your code here
+
+from collections.abc import Hashable
+
+object_list = [1, 397, 27468, -95, 1309, 397, -539874, -240767, -95, 397]
+
+total = []
+hashes =
+
+for i in object_list:
+    if isinstance(i, Hashable):
+
+    else:
+        total = 0
+
+print(total, len(total))
