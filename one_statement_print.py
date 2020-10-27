03df7a12d40ee693be050cd0cd1bdb11a3612178
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-10-27 16:24:38 -0300

    one statement print()

---
+++ one_statement_print.py
@@ -0,0 +1,15 @@
+# Write a snippet that will print the following lines using only one print() statement.
+# The lines:
+
+# Night, square, apothecary, lantern,
+# Its meaningless and pallid light.
+# Return a half a lifetime after –
+# All will remain. A scapeless rite.
+
+line1 = "Night, square, apothecary, lantern,"
+line2 = "Its meaningless and pallid light."
+line3 = "Return a half a lifetime after – "
+line4 = "All will remain. A scapeless rite."
+
+if __name__ == "__main__":
+    print(line1, line2, line3, line4, sep="\n")
