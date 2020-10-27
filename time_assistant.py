43472870706d2b410c821ce469062a8b787519a7
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-10-27 15:39:49 -0300

    typo fixed

--- time_assistant.py
+++ time_assistant.py
@@ -1,5 +1,6 @@
 # You are trying to write a personal assistant, and one of its tasks is to print the local time for a user.
 # Write a piece of code that would do that using the strftime() function with the output format: "It's 09:11.".
+
 from time import strftime, localtime
 
 if __name__ == "__main__":
