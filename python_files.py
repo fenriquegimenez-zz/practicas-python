5c3f92d8e6c7db4a6944cce6a8fd2e911ba85fe0
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-10-30 16:06:46 -0300

    Open the file

---
+++ python_files.py
@@ -0,0 +1,8 @@
+# Create a file "stars.txt" and open it for writing as a text in the UTF-16 encoding.
+# Name the variable test_file.
+
+# Don't forget to close the file!
+
+test_file = open('stars.txt', mode='a', encoding='utf-16')
+
+test_file.close()
